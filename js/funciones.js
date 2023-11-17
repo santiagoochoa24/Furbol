73ef48411d54558aeb84ca731853ad90b23ed449
santiagoochoa24 <151106175+santiagoochoa24@users.noreply.github.com>
2023-11-16 20:57:50 -0600

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,36 @@
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const close = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () =>{
+
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () =>{
+
+    nav.classList.remove("visible");
+})
+
+
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_4uvussr';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Enviado!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
